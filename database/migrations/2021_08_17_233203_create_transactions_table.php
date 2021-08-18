98ca69ae664f98e02b27595e95bbcb062ac70ed4
M-Hidayatullah <hidayatullahblk@gmail.com>
2021-08-18 10:58:43 +0800

    in process

---
+++ database/migrations/2021_08_17_233203_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->double('amount');
+            $table->timestamp('time')->default(now());
+            $table->enum('type', ['expense', 'revenue']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
